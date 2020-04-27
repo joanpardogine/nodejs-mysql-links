a8654d4f772cea2db42f660f79fba3e271446dee
joanpardogine <joanpardo@ginebro.cat>
2020-04-27 10:48:53 +0200

    try solucionar import timeago from 'timeago.js';

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -1,4 +1,5 @@
-const timeago = require('timeago.js');
+import timeago from 'timeago.js';
+//const timeago = require('timeago.js');
 const timeagoInstance = timeago();
 
 const helpers = {};
@@ -7,4 +8,4 @@ helpers.timeago = (savedTimestamp) => {
     return timeagoInstance.format(savedTimestamp);
 };
 
-module.exports = helpers;
+export default helpers;
