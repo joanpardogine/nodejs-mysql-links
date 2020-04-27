f7a69fac83fcdd8428efc05eadfac077e34c2b49
joanpardogine <joanpardo@ginebro.cat>
2020-04-27 11:15:28 +0200

    Ja funciona 100%

--- src/keys.js
+++ src/keys.js
@@ -5,7 +5,7 @@ module.exports = {
         host: process.env.DATABASE_HOST || 'bhj4waxt4p73hge7a5gf-mysql.services.clever-cloud.com',
         user: process.env.DATABASE_USER || 'uvk4sp9ijlt4xnuv',
         password: process.env.DATABASE_PASSWORD || 'x8At56PP1eLoKz0EvWQz',
-        database: process.env.DATABASE_NAME || 'db_links'
+        database: process.env.DATABASE_NAME || 'bhj4waxt4p73hge7a5gf'
     }
 
 };
