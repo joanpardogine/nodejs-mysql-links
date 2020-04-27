05da844a7a6c0c1e6b0590010bc862e518c668ea
joanpardogine <joanpardo@ginebro.cat>
2020-04-27 10:26:41 +0200

    modificats keys apuntant a clever-cloud

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: process.env.DATABASE_HOST || 'localhost',
-        user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || '',
+        host: process.env.DATABASE_HOST || 'bhj4waxt4p73hge7a5gf-mysql.services.clever-cloud.com',
+        user: process.env.DATABASE_USER || 'uvk4sp9ijlt4xnuv',
+        password: process.env.DATABASE_PASSWORD || 'x8At56PP1eLoKz0EvWQz',
         database: process.env.DATABASE_NAME || 'db_links'
     }
 
